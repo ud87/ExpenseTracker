91fb4c9b7c95a6805ec0464222594b153531e38e
Udaya <udayarai@hotmail.co.uk>
2025-07-26 09:13:17 +0100

    CategoryController with its view and action method create, edit, delete added

--- ExpenseTrackerApp/ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTrackerApp/ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccaca485881958c456795eff5384514dabdb213")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
