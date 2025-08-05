1e7566b9a673e3cb65b304b0ed57511718e5b6fa
Udaya <udayarai@hotmail.co.uk>
2025-08-05 23:05:52 +0100

    Sync fusion grid colum template added to Type so that expense has red background and income has green background

--- ExpenseTrackerApp/ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTrackerApp/ExpenseTracker/obj/Debug/net9.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a5ca2885369066470b62502e3dea5fb73a6466")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f50157a09fecaa272304697a213de0f88112214")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
